86055c4e78819ae6be2bfc61256d2814439d4abc
Romit <romitgabani@icloud.com>
2026-01-31 02:04:33 +0530

    remove registration

--- app/javascript/packs/url_redirect_download.js
+++ app/javascript/packs/url_redirect_download.js
@@ -2,9 +2,8 @@ import ReactOnRails from "react-on-rails";
 
 import BasePage from "$app/utils/base_page";
 
-import { WithContent as DownloadPageWithContent } from "$app/components/DownloadPage/WithContent";
 import DownloadPageWithoutContent from "$app/components/server-components/DownloadPage/WithoutContent";
 
 BasePage.initialize();
 
-ReactOnRails.default.register({ DownloadPageWithContent, DownloadPageWithoutContent });
+ReactOnRails.default.register({ DownloadPageWithoutContent });
